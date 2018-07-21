4ce861298a38f87d8affb0cbbee7c63e7d3e830f
oushiming <oushiming@sxt.com>
2018-07-21 10:44:26 +0800

    网关权限

--- src/main/java/com/hmily/product/controller/ProductController.java
+++ src/main/java/com/hmily/product/controller/ProductController.java
@@ -70,4 +70,9 @@ public class ProductController {
         return ResultVOUtil.success(productVOList);
     }
 
+    @GetMapping("/test")
+    public ResultVO test(){
+        return list();
+    }
+
 }
